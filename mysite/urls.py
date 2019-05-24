d85b8d3f2aa8aa93e6868614a8f44471162e11fb
Gloria Higley <gloria.ann.higley@gmail.com>
2019-05-23 23:05:48 -0500

    add blog template

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-#from django.conf.urls import url
-#from django.contrib import admin
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
